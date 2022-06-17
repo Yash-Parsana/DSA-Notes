0a4a3dfc4c630533b479a25a6d85b7e18b799a9f
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-17 16:21:41 +0530

    Time: 272 ms (42.14%), Space: 77.3 MB (59.43%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -7,9 +7,9 @@ public:
         
         stack<int> s;
         int i=0;
-        while(i<v.size())
+        while(i<=v.size())
         {
-            while(!s.empty()&&v[s.top()]>v[i])
+            while(!s.empty()&&(i==v.size()||v[s.top()]>v[i]))
             {
                 int curr=s.top();s.pop();
                 
@@ -19,13 +19,7 @@ public:
             s.push(i);
             i++;
         }
-        while(!s.empty())
-        {
-            int curr=s.top();s.pop();
 
-            if(s.empty()) ans=max(ans,(i)*v[curr]);                                   else ans=max(ans,(i-s.top()-1)*v[curr]);
-
-        }
         return ans;
         
     }
