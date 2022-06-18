fcb3d4babd9fd25d4af46e12265abff7607d0231
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-18 17:56:03 +0530

    Time: 448 ms (10.40%), Space: 86.1 MB (22.78%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,20 +1,21 @@
 class StockSpanner {
 public:
     
-    stack<pair<int,int>>st;
+    stack<int> s;
+    vector<int> v;
+    
     StockSpanner() {
-        
     }
     
-    int next(int price) {
-        int count = 1;
-        while(!st.empty()&& st.top().first <= price){
-            count = count + st.top().second;
-            st.pop();
-        }
-        st.push({price,count});
-        return count;
-    
+    int next(int p) {
+        int ans=0;
+        v.push_back(p);
+        while(!s.empty()&&v[s.top()]<=p) s.pop();
+        if(s.empty()) ans= v.size();
+        else  ans=v.size()-s.top()-1;
+        s.push(v.size()-1);
+        
+        return ans;
     }
 };
 
