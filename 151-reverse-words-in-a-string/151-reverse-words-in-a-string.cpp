1ab5396c82fa64d57e887351d56947b80cfbdefa
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-21 17:49:15 +0530

    Time: 62 ms (5.10%), Space: 67.1 MB (5.23%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,50 +1,59 @@
 
 class Solution {
 public:
-string reverseWords(string str) {
-        str.append(1,' ');
-    int i,n=str.length();
-    int low=0;
-    int high;
-    for(i=0;i<n;)
-    {
-        while(str[i]==' ' and i<n)
-        i++;
-        if(i>=n)
-            break;
-        low=i;
-        high=i;
-        while(str[high]!=' ' and high<n)
-        high++;
-        int t=high;
-        high--;
-        while(low<high)
+    string reverseWords(string s) {
+  
+        
+        for(int z=0;z<s.size();z++)
         {
-            swap(str[low],str[high]);
-            low++;
-            high--;
+            int k=0;
+            while(s[z]==' ')z++,k++;
+            
+            if(k>1)
+            {
+                s=s.substr(0,z-k+1)+s.substr(z);
+                z=z-k;
+            }
         }
-        i=t;
-    }
-    reverse(str.begin(),str.end());
-    int start=0;
-    for(i=0;i<n;)
-    {
-        while(str[i]==' ' and i<n)
-        i++;
-        if(i>=n)
-            break;
-        int id=i;
-        while(str[id]!=' ' and id<n)
+        
+        
+        int i=0,j=s.size()-1;
+        
+        
+        while(i<j)
         {
-            str[start]=str[id];
-            start++;
-            id++;
+            swap(s[i],s[j]);
+            i++;j--;
         }
-        str[start]=' ';
-        start++;
-        i=id;
-    }
-    return str.substr(0,start-1);
+        
+        i=0;j=0;
+        
+        for(int z=0;z<=s.size();z++)
+        {
+            if(z==s.size()||s[z]==' '){
+            
+            j=z-1;
+            while(i<j)
+            {
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+            i=z+1;
+            }
+        }
+        
+        i=0;j=0;
+        
+        while(s[i]==' ')i++;
+        
+        s=s.substr(i);
+        
+        i=s.size()-1;
+        while(s[i]==' ')i--;
+        
+        s=s.substr(0,i+1);
+        
+        return s;
     }
 };
