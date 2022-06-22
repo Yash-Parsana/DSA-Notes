2dff1244c2a54783e4e5d02262fcbdce01b7e28b
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-22 12:01:02 +0530

    Time: 4 ms (88.07%), Space: 6.6 MB (55.89%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,13 +1,15 @@
+vector<string> dp(31,"");
 class Solution {
 public:
 string countAndSay(int n) {
 
     if(n==1)
-        return "1";
+        return dp[1]="1";
 
     if(n==2)
-        return "11";
-
+        return dp[2]= "11";
+    if(!dp[n].empty()) return dp[n];
+    
     string str = countAndSay(n - 1);
 
     string ans="";
@@ -24,6 +26,6 @@ string countAndSay(int n) {
         if(count>0)
             ans += to_string(count) + str[z];
     }
-    return ans;
+    return dp[n]= ans;
 }
 };
