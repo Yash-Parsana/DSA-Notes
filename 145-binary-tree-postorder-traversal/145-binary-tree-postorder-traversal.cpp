5b66934e0d340fc8439fbb4e7a5a58f79f421118
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-22 18:42:33 +0530

    Time: 5 ms (23.14%), Space: 8.3 MB (95.81%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -6,31 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNod
+    e(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    
-    void postorder(TreeNode* root,vector<int> &ans)
-    {
-        if(!root)return;
-        
-        postorder(root->left,ans);
-        postorder(root->right,ans);
-        ans.push_back(root->val);
-        
-    }
-    
     vector<int> postorderTraversal(TreeNode* root) {
-        
-        vector<int>ans;
-        
-        postorder(root,ans);
-        
+
+        vector<int> ans;
+        stack<TreeNode*> s;
+        TreeNode* curr=root;
+        while(!s.empty()||curr)
+        {
+            if(curr)
+            {
+                s.push(curr);
+                curr=curr->left;
+            }
+            else
+            {
+                TreeNode* temp=s.top()->right;
+                
+                if(!temp)
+                {
+                    temp=s.top();s.pop();
+                    ans.push_back(temp->val);
+                    while(!s.empty()&&s.top()->right==temp)
+                    {
+                        temp=s.top();s.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else curr=temp;
+            }
+        }
         return ans;
-        
-        
         
     }
 };
