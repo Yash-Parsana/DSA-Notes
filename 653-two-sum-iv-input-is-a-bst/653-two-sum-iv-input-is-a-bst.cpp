80c551be50362e444750b377f5536e53a5630808
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-26 00:27:44 +0530

    Time: 53 ms (68.13%), Space: 36.7 MB (80.38%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -12,28 +12,54 @@
 class Solution {
 public:
     
-    void inorder(TreeNode* root,vector<int> &v)
+    void pushLeft(TreeNode* root,stack<TreeNode*> &s)
     {
-        if(!root)return;
+        while(root)
+        {
+            s.push(root);
+            root=root->left;
+        }
         
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
+    }
+    void pushRight(TreeNode* root,stack<TreeNode*> &s)
+    {
+        while(root)
+        {
+            s.push(root);
+            root=root->right;
+        }
+    }
+    int front(stack<TreeNode*> &s)
+    {
+        TreeNode* curr=s.top();
+        s.pop();
+        pushLeft(curr->right,s);
+        return curr->val;
+    }
+    int back(stack<TreeNode*> &s)
+    {
+        TreeNode* curr=s.top();
+        s.pop();
+        pushRight(curr->left,s);
+        return curr->val;
     }
     
     bool findTarget(TreeNode* root, int k) {
         
-        vector<int> v;
+        stack<TreeNode*>l,r;
         
-        inorder(root,v);
+        pushLeft(root,l);
+        pushRight(root,r);
         
-        int i=0,j=v.size()-1;
+        int i=front(l),j=back(r);
+        
         while(i<j)
         {
-            if(v[i]+v[j]==k)return 1;
-            else if(v[i]+v[j]<k)i++;
-            else j--;
+            if(i+j==k)return 1;
+            else if(i+j>k)j=back(r);
+            else i=front(l);
         }
         return 0;
+        
     }
 };
