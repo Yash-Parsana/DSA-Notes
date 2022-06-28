05b439dbc5d1aff981639168bdbf8533b48e2cf6
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-06-28 15:17:30 +0530

    Time: 154 ms (5.02%), Space: 38.5 MB (5.20%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool Bipartite(int curr,bool c,vector<vector<int>> adj,vector<bool>&vis,vector<bool> &color)
+    {
+        vis[curr] = 1;
+        color[curr] = c;
+
+        for(int i:adj[curr])
+        {
+            if(vis[i]&&color[i]==c)
+            {
+                return 0;
+            }
+            if(!vis[i]&&!Bipartite(i, !c, adj, vis, color))
+                return 0;
+        }
+        return 1;
+    }
+
+    bool isBipartite(vector<vector<int>>& adj) {
+        
+        int n=adj.size();
+        vector<bool> vis(n,0), color(n,0);
+
+        
+        for (int z = 0; z < n;z++)
+        {
+            if(!vis[z]&&!Bipartite(z,0,adj,vis,color))
+                return 0;
+        }
+        return 1;
+    }
+};
