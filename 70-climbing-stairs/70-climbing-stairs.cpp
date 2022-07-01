8a56ac5d0b5c392704392a1448eaefac7a79e267
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-02 00:04:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int totalSteps(int n,vector<int> &dp)
+        int totalSteps(int n,vector<int> &dp)
     {
         if(n<0)return 0;
         if(n==0)
@@ -20,4 +20,17 @@ public:
         return dp[n];
         
     }
+//     int climbStairs(int n) {
+        
+//         if(n==0||n==1)return n;
+//         vector<int> dp(n+1,0);
+//         dp[0]=0;
+//         dp[1]=1;
+        
+//         for(int z=2;z<=n;z++)
+//         {
+//             dp[z]=dp[z-1]+dp[z-2]+1;
+//         }
+//         return dp[n];
+//     }
 };
