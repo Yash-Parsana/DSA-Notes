fe33f9cc95b40ca6711c7fee9c0086c836a1a76a
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-02 00:08:19 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (30.74%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     
-        int totalSteps(int n,vector<int> &dp)
-    {
-        if(n<0)return 0;
-        if(n==0)
-        {
-            return 1;
-        }
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=totalSteps(n-1,dp)+totalSteps(n-2,dp);
-    }
+//         int totalSteps(int n,vector<int> &dp)
+//     {
+//         if(n<0)return 0;
+//         if(n==0)
+//         {
+//             return 1;
+//         }
+//         if(dp[n]!=-1)return dp[n];
+//         return dp[n]=totalSteps(n-1,dp)+totalSteps(n-2,dp);
+//     }
     
-    int climbStairs(int n) {
-        
-       
-        vector<int>dp(n+1,-1);
-        totalSteps(n,dp);
-        return dp[n];
-        
-    }
 //     int climbStairs(int n) {
         
-//         if(n==0||n==1)return n;
-//         vector<int> dp(n+1,0);
-//         dp[0]=0;
-//         dp[1]=1;
-        
-//         for(int z=2;z<=n;z++)
-//         {
-//             dp[z]=dp[z-1]+dp[z-2]+1;
-//         }
+       
+//         vector<int>dp(n+1,-1);
+//         totalSteps(n,dp);
 //         return dp[n];
+        
 //     }
+    int climbStairs(int n) {
+        
+        if(n<=2)return n;
+        vector<int> dp(n+1,0);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int z=3;z<=n;z++)
+        {
+            dp[z]=dp[z-1]+dp[z-2];
+        }
+        return dp[n];
+    }
 };
