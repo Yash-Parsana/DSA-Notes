76bfb8e1de4fd09527040c82509d7301bf7063e9
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-02 21:51:05 +0530

    Time: 12 ms (69.22%), Space: 10.1 MB (51.59%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    
-    int solve(vector<vector<int>> &v,int &n,int &m,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i==n-1&&j==m-1)return v[n-1][m-1];
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int ans=1e8;
-        if(i+1<n)
-            ans=min(ans,solve(v,n,m,i+1,j,dp)+v[i][j]);
-        if(j+1<m)
-            ans=min(ans,solve(v,n,m,i,j+1,dp)+v[i][j]);
-        return dp[i][j]=ans;
-    }
-    
-    
     int minPathSum(vector<vector<int>>& v) {
         
-        int n=v.size();
-        int m=v[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int n=v.size(),m=v[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
         
-        return solve(v,n,m,0,0,dp);
+        for(int z=1;z<n;z++)
+        {
+            v[z][0]+=v[z-1][0];
+        }
+        for(int z=1;z<m;z++)
+        {
+            v[0][z]+=v[0][z-1];
+        }
         
+        for(int z=1;z<n;z++)
+        {
+            for(int y=1;y<m;y++)
+            {
+                v[z][y]=min(v[z-1][y],v[z][y-1])+v[z][y];
+            }
+        }
+        return v[n-1][m-1];
     }
 };
