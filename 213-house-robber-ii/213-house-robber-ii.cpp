0df55416789505c5b7fbc2b5bd70a230586bdb21
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-02 16:57:10 +0530

    Time: 13 ms (5.21%), Space: 8 MB (17.80%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     
-    int solve(vector<int> &v,vector<int> &dp,int i,int n)
-    {
-        if(i>=n)
+    int rob(vector<int>& nums,int i,int n) {
+        
+        vector<int> dp(n,0);
+        
+        dp[i]=nums[i];
+        dp[i+1]=max(dp[i],nums[i+1]);
+        
+        
+        for(int z=i+2;z<n;z++)
         {
-            return 0;
+            dp[z]=max(dp[z-2]+nums[z],dp[z-1]);
         }
-        if(dp[i]!=-1)
-            return dp[i];
-
-        return dp[i]=max(solve(v, dp, i + 1,n), solve(v, dp, i + 2,n) + v[i]);
+        return dp[n-1];
     }
     
     int rob(vector<int>& nums) {
         
-        
         int n=nums.size();
-        
         if(n==1)return nums[0];
         if(n==2)return max(nums[0],nums[1]);
+        if(n==3)return max({nums[0],nums[1],nums[2]});
         
+        return max(rob(nums,0,n-1),rob(nums,1,n));
         
-        vector<int> dp(n,-1);
-        int ans1=solve(nums,dp,0,n-1);
-        dp=vector<int>(n,-1);
-        
-        int ans2=solve(nums,dp,1,n);           
-        return max(ans1,ans2);
     }
-    
-
 };
