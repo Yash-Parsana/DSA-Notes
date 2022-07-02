f6987c781d5e4f67a12ff615e49ee894dcf61acd
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-02 23:14:50 +0530

    Time: 8 ms (66.65%), Space: 8.7 MB (47.96%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,28 +3,27 @@ public:
     int minimumTotal(vector<vector<int>>& v) {
         
         int n=v.size();
-        vector<vector<int>> dp(n,vector<int>(n,1e5));
+        vector<int> dp(n,1e5);
         
-        dp[0][0]=v[0][0];
-        for(int z=1;z<n;z++)
-        {
-            dp[z][0]=dp[z-1][0]+v[z][0];
-        }
+        dp[0]=v[0][0];
         
         for(int z=1;z<n;z++)
         {
-            for(int y=1;y<=z;y++)
+        
+            for(int y=z;y>=0;y--)
             {
-                dp[z][y]=min(dp[z-1][y],dp[z-1][y-1])+v[z][y];
+                if(y==0)dp[y]+=v[z][y];
+                else
+                    dp[y]=min(dp[y],dp[y-1])+v[z][y];
             }
+            
         }
-        
-
         
         int ans=1e5;
+        
         for(int z=0;z<n;z++)
         {
-            ans=min(ans,dp[n-1][z]);
+            ans=min(ans,dp[z]);
         }
         return ans;
     }
