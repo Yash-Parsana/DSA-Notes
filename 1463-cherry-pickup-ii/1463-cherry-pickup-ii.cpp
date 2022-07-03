bf9efc84bb721438fd4dc0576102a40a86711348
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-04 00:34:25 +0530

    Time: 333 ms (6.11%), Space: 16.8 MB (6.18%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& v) {
         int n=v.size(),m=v[0].size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        vector<vector<int>> dp(m,vector<int>(m,0));
         
         for(int z=0;z<m;z++)
         {
             for(int y=0;y<m;y++)
             {
-                if(z==y)dp[n-1][z][y]=v[n-1][y];
-                else dp[n-1][z][y]=v[n-1][z]+v[n-1][y];
+                if(z==y)dp[z][y]=v[n-1][y];
+                else dp[z][y]=v[n-1][z]+v[n-1][y];
             }
         }
         
         for(int z=n-2;z>=0;z--)
         {
+            vector<vector<int>> temp(m,vector<int>(m,-1));
+
             for(int y=0;y<m;y++)
             {
                 for(int w=0;w<m;w++)
@@ -24,16 +26,19 @@ public:
                         for(int j=-1;j<2;j++)
                         {
                             if(y+i<0||y+i>=m||w+j<0||w+j>=m)continue;
-                            dp[z][y][w]=max(dp[z][y][w],dp[z+1][y+i][w+j]);
+                            temp[y][w]=max(temp[y][w],dp[y+i][w+j]);
                         }
                     }
                     if(y==w)
-                        dp[z][y][w]+=v[z][w];
-                    else dp[z][y][w]+=v[z][w]+v[z][y];
+                        temp[y][w]+=v[z][w];
+                    else temp[y][w]+=v[z][w]+v[z][y];
+                
                 }
             }
+            swap(dp,temp);
+
         }
         
-        return dp[0][0][m-1];
+        return dp[0][m-1];
     }
 };
