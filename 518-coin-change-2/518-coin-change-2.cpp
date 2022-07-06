f7367ddd87b29aa062eb0125134d3fb2e6b86195
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 15:09:15 +0530

    Time: 96 ms (14.67%), Space: 18.3 MB (46.31%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     
-    int solve(vector<int>&coins,vector<vector<int>>&dp,int i,int &n,int target)
-    {
-        if(i>=n)return 0;
-        if(target==0)return 1;
-        
-        if(dp[i][target]!=-1)return dp[i][target];
-        
-        int take=0;
-        if(coins[i]<=target)
-            take=solve(coins,dp,i,n,target-coins[i]);
-        
-        return dp[i][target]=take+solve(coins,dp,i+1,n,target);
-    }
-    
-    
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
         
-        return solve(coins,dp,0,n,amount);
+        for(int z=0;z<=amount;z++)
+        {
+            if(z%coins[0]==0)
+                dp[0][z]=1;
+        }
         
+        for(int z=1;z<n;z++)
+        {
+            for(int y=0;y<amount+1;y++)
+            {
+                int take=0;
+                if(coins[z]<=y)
+                    take=dp[z][y-coins[z]];
+                int dontTake=dp[z-1][y];
+                
+                dp[z][y]=take+dontTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
