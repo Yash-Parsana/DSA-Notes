5f1ad38ba270bf6e68cb1881cbb2ff0d91438840
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 13:18:15 +0530

    Time: 18 ms (68.04%), Space: 12.3 MB (39.55%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
     
-    int solve(int i,int &n,vector<int>&v,vector<unordered_map<int,int>> &dp,int target)
+//     s1-s2=target; s1=element with + sign ,s2= element with - sign
+//     s1+s2=sum;
+//     s1=(target+sum)/2;
+    
+//     Problem :- Find all subsequence which sum = s1
+    
+    int solve(vector<int>&v,int i,int &n,vector<vector<int>>&dp,int s)
     {
         if(i>=n)
         {
-            if(target==0)return 1;
+            if(s==0)return 1;
             return 0;
         }
+        if(s==0&&v[i]!=0)return 1;
+
+        if(dp[i][s]!=-1)return dp[i][s];
         
-        if(dp[i].find(target)!=dp[i].end())return dp[i][target];
+        int ans=0;
+        if(v[i]<=s)ans=solve(v,i+1,n,dp,s-v[i]);
         
-        return dp[i][target]=solve(i+1,n,v,dp,target-v[i])+solve(i+1,n,v,dp,target+v[i]);
+        return dp[i][s]=solve(v,i+1,n,dp,s)+ans;
     }
     
-    
     int findTargetSumWays(vector<int>& nums, int target) {
+        
+        sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<unordered_map<int,int>>dp(n);
-      
-        return solve(0,n,nums,dp,target);
+        int sum=0;
+        sum=accumulate(nums.begin(),nums.end(),sum);
+
+        if(abs(target)>sum)return 0;
+        if((sum+target)%2)return 0;
+        sum=(sum+target)/2;
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+ 
+        
+        return solve(nums,0,n,dp,sum);
         
     }
 };
