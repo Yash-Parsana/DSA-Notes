b7780c9ddd2383d957007a10b160376f5f00d047
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 14:43:13 +0530

    Time: 19 ms (65.72%), Space: 9.5 MB (50.37%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -17,30 +17,24 @@ public:
         if(abs(target)>sum)return 0;
         if((sum+target)%2)return 0;
         sum=(sum+target)/2;
-        vector<vector<int>> dp(n,vector<int>(sum+1,0));
+        vector<int> dp(sum+1,0);
 
-        dp[0][0]=1;
-        if(nums[0]<=sum)dp[0][nums[0]]++;
-        // for(int z=0;z<=sum;z++)
-        // {
-        //     cout<<dp[0][z]<<" ";
-        // }
-        // cout<<endl;
+        dp[0]=1;
+        if(nums[0]<=sum)dp[nums[0]]++;
+
         for(int z=1;z<n;z++)
         {
-            for(int y=0;y<=sum;y++)
+            for(int y=sum;y>=0;y--)
             {
                 int take=0;
                 if(y>=nums[z])
-                    take=dp[z-1][y-nums[z]];
-                int dontTake=dp[z-1][y];
+                    take=dp[y-nums[z]];
+                int dontTake=dp[y];
                 
-                dp[z][y]=take+dontTake;
-                // cout<<dp[z][y]<<" ";
+                dp[y]=take+dontTake;
             }
-            // cout<<endl;
         }
         
-        return dp[n-1][sum];
+        return dp[sum];
     }
 };
