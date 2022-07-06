b455ea2c7b34783ac9406d45327c756f22c51a41
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 18:36:43 +0530

    Time: 32 ms (55.83%), Space: 13 MB (40.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     
-    int solve(string &s1,string &s2,int i,int j,vector<vector<int>> &dp)
-    {
-        if(i>=s1.size()||j>=s2.size())return 0;
-        
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int take=0;
-            if(s1[i]==s2[j])take=solve(s1,s2,i+1,j+1,dp)+1;
-        int dontTake=max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
-        
-        return dp[i][j]=max(take,dontTake);
-        
-    }
-    
     int longestCommonSubsequence(string s1, string s2) {
         
         vector<vector<int>> dp(s1.size(),vector<int>(s2.size(),-1));
-        return solve(s1,s2,0,0,dp);
+
+        bool b=0;
+        for(int z=0;z<s1.size();z++)
+        {
+            if(s1[z]==s2[0])b=1;
+
+            dp[z][0]=b;
+        }
+        b=0;
+        for(int z=0;z<s2.size();z++)
+        {
+            if(s2[z]==s1[0])b=1;
+            dp[0][z]=b;
+        }
+        
+        for(int z=1;z<s1.size();z++)
+        {
+            for(int y=1;y<s2.size();y++)
+            {
+                int dontTake=max(dp[z][y-1],dp[z-1][y]);
+                
+                int take=0;
+                if(s1[z]==s2[y])
+                    take=dp[z-1][y-1]+1;
+                dp[z][y]=max(take,dontTake);
+            }
+        }
+        
+        return dp[s1.size()-1][s2.size()-1];
     }
 };
