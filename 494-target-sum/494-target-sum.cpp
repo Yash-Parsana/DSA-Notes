13f0d66114d80941d544e839fb36e156d7452d52
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 11:53:16 +0530

    Time: 453 ms (27.33%), Space: 61 MB (13.07%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int solve(int i,int &n,vector<int>&v,vector<unordered_map<int,int>> &dp,int target)
+    {
+        if(i>=n)
+        {
+            if(target==0)return 1;
+            return 0;
+        }
+        
+        if(dp[i].find(target)!=dp[i].end())return dp[i][target];
+        
+        return dp[i][target]=solve(i+1,n,v,dp,target-v[i])+solve(i+1,n,v,dp,target+v[i]);
+    }
+    
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<unordered_map<int,int>>dp(n);
+        return solve(0,n,nums,dp,target);
+        
+    }
+};
