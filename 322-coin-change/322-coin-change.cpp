2436bd544f14ed24c5b45f59be15f356f28f6fd1
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-06 11:30:22 +0530

    Time: 153 ms (39.17%), Space: 45.2 MB (21.61%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &coins,vector<vector<int>>&dp,int i,int v,int &n)
-    {
-        if(i>=n)return 1e5;
-        if(v==0)return 0;
-        
-        if(dp[i][v]!=-1)return dp[i][v];
-        
-        int ans=1e5;
-        if(v>=coins[i])
-            ans=solve(coins,dp,i,v-coins[i],n)+1;
-        
-        return dp[i][v]= min(ans,solve(coins,dp,i+1,v,n));
-    
-    }
-    
-    
     int coinChange(vector<int>& coins, int amount) {
         
         int n=coins.size();
-        sort(coins.begin(),coins.end(),greater<int>());
         
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
         
-        int ans=solve(coins,dp,0,amount,n);
-        if(ans==1e5)return -1;
-        return ans;
+        for(int z=0;z<amount+1;z++)
+        {
+            if(z%coins[0]==0)dp[0][z]=z/coins[0];
+            else dp[0][z]=1e5;
+        }
+
+        for(int z=1;z<n;z++)
+        {
+            for(int y=0;y<=amount;y++)
+            {
+                int dontTake=dp[z-1][y];
+                int take=INT_MAX;
+                if(y>=coins[z])
+                    take=dp[z][y-coins[z]]+1;
+                
+                dp[z][y]=min(take,dontTake);
+                // cout<<dp[z][y]<<" ";
+            }
+        }
         
+        if(dp[n-1][amount]==1e5)return -1;
+        return dp[n-1][amount];
     }
 };
