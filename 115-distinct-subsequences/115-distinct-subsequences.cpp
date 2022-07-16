2c8f9458a9a2dc7c995b8caa3a54c2f94e077643
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-16 14:41:53 +0530

    Time: 37 ms (82.02%), Space: 30.2 MB (17.06%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    
-    int solve(string &s,int i,int j,string &t,vector<vector<int>>&dp)
-    {
-        if(j==t.size()-1)return 1;
-        if(i>=s.size())return 0;
-    
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        int ans=0;
-        if(s[i]==t[j])
-        {
-            for(int z=i+1;z<s.size();z++)
-            {
-                if(t[j+1]==s[z]&&s.size()-z>=t.size()-j-1)
-                {
-                    ans+=solve(s,z,j+1,t,dp);
-                }
-                
-            }
-        }
-        return dp[i][j]=ans;
-    }
-    
     int numDistinct(string s, string t) {
         
-        int ans=0;
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
-
-        for(int z=0;z<s.size();z++)
+        int n=s.size(),m=t.size();
+        vector<vector<long long>> dp(n,vector<long long>(m,0));
+        
+        int temp=0;
+        for(int z=0;z<n;z++)
         {
-            if(s[z]==t[0]&&s.size()-z>=t.size())
-                ans+=solve(s,z,0,t,dp);
+            if(s[z]==t[0])
+            {
+                temp++;
+            }
+            dp[z][0]=temp;
         }
-        return ans;
+        
+        for(int z=1;z<n;z++)
+        {
+            for(int y=1;y<m;y++)
+            {
+                if(s[z]==t[y]&&s.size()-z>=t.size()-y)
+                {
+                    dp[z][y]=dp[z-1][y-1]+dp[z-1][y];
+                }
+                else{
+                    dp[z][y]=dp[z-1][y];
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
