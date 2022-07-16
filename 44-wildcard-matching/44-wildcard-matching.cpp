3cae0211e0fc220b195bd07bef8beccbe16ecb91
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-16 17:18:30 +0530

    Time: 108 ms (40.58%), Space: 11.4 MB (54.09%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,42 +1,10 @@
 class Solution {
 public:
-    
-    bool solve(string &s,string &p,int i,int j,vector<vector<int>> &dp)
-    {
-        if(i>=s.size())
-        {
-            if(j>=p.size())return 1;
-            while(j<p.size())
-            {
-                if(p[j]!='*')return 0;
-                j++;
-            }
-            return 1;
-        }
-        if(j>=p.size())
-        {
-            return 0;
-        }
-    
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-        if(s[i]==p[j]||p[j]=='?')
-            return dp[i][j]=solve(s,p,i+1,j+1,dp);
-
-        if(p[j]=='*')
-        {
-            bool b=(solve(s,p,i+1,j,dp)||solve(s,p,i,j+1,dp));
-            return dp[i][j]=b;
-        }
-        
-        return dp[i][j]=0;
-        
-        
-    }
-    
     bool isMatch(string s, string p) {
+     
         
-        if(s.size()==0)
+        if(s.empty()&&p.empty())return 1;
+        if(s.empty())
         {
             for(int z=0;z<p.size();z++)
             {
@@ -44,12 +12,51 @@ public:
             }
             return 1;
         }
-        if(s.empty()&&p.empty())return 1;
+        if(p.empty())return 0;
         
-        if(p.empty()&&!s.empty())return 0;
+        vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,0));
         
-        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
-        return solve(s,p,0,0,dp);
-        
+        if(p[0]==s[0]||p[0]=='?')
+        {
+            dp[0][0]=1;
+        }
+        else if(p[0]=='*')
+        {
+            for(int z=0;z<s.size();z++)
+            {
+                dp[z][0]=1;
+            }
+
+
+        }
+        bool b=0;
+        for(int z=0;z<p.size();z++)
+        {
+            if(!b&&(p[z]==s[0]||p[z]=='?')){
+                dp[0][z]=1;b=1;
+            }
+            else if(p[z]=='*')
+            {
+                dp[0][z]=1;                    
+            }
+            else break;
+        }
+
+        for(int z=1;z<s.size();z++)
+        {
+            for(int y=1;y<p.size();y++)
+            {
+                if(s[z]==p[y]||p[y]=='?')
+                {
+                    dp[z][y]=dp[z-1][y-1];
+                }
+                else if(p[y]=='*')
+                {
+                    dp[z][y]=dp[z-1][y]||dp[z][y-1];
+                }
+            }
+        }
+
+        return dp[s.size()-1][p.size()-1];
     }
 };
