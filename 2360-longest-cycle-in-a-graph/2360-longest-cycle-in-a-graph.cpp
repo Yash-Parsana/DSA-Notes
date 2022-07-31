8fab6873175967131c3cb660da5877fb9a20a20f
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-07-31 11:21:34 +0530

    Time: 342 ms (100.00%), Space: 118.3 MB (100.00%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-int dfs(int i, int from, int dist, vector<int>& e, vector<pair<int, int>>& memo) {
-    if (i != -1) {
-        auto [dist_i, from_i] = memo[i];
-        if (dist_i == -1) {
-            memo[i] = {dist, from};
-            return dfs(e[i], from, dist + 1, e, memo);
+    
+    int dfs(vector<int>&v,int from,int temp,int curr,vector<pair<int,int>>&dist)
+    {
+        if(curr==-1)return -1;
+        
+        int f=dist[curr].first;
+        int d=dist[curr].second;
+        
+        if(d==-1)
+        {
+            dist[curr]={from,temp};
+            return dfs(v,from,temp+1,v[curr],dist);
         }
-        if (from_i == from)
-            return dist - dist_i;
+        if(from==f)return temp-d;
+        
+        return -1;
+        
     }
-    return -1;
-}    
-int longestCycle(vector<int>& e) {
-    int res = -1;
-    vector<pair<int, int>> memo(e.size(), {-1, -1});
-    for (int i = 0; i < e.size(); ++i)
-        res = max(res, dfs(i, i, 0, e, memo));
-    return res;
-}
+    
+    int longestCycle(vector<int>& v) {
+        
+        int ans=-1,n=v.size();
+        vector<pair<int,int>>dist(n,{-1,-1});
+        
+        for(int z=0;z<n;z++)
+        {
+            ans=max(ans,dfs(v,z,0,z,dist));
+        }
+        return ans;
+    }
 };
