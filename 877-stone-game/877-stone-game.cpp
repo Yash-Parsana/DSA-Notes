863fb0e9477d9befac6e7e86759960d1a67dc313
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-03 18:27:05 +0530

    Time: 69 ms (8.92%), Space: 16.5 MB (21.60%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    
+    int solve(vector<int>&v,vector<vector<int>>&dp,int i,int j)
+    {
+        if(i>j)return 0;
+        if(i==j)return v[i];
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int left=v[i]-solve(v,dp,i+1,j);
+        int right=v[j]-solve(v,dp,i,j-1);
+        
+        return dp[i][j]=max(left,right);
+        
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+        return solve(piles,dp,0,n-1)>0;
     }
 };
