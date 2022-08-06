7335c94e45782740367853f5d6d196ba6b94cec9
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-06 13:34:10 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (58.40%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        int pig=0;
+        int base=minutesToTest/minutesToDie+1,temp=1;
+        int ans=0;
+        while(temp<buckets)
+        {
+            ans++;
+            temp*=base;
+        }
+        
+        return ans;
+    }
+}
