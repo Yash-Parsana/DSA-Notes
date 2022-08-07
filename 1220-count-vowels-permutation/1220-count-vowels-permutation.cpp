cab38fb8f390e76c70145ae0db226bb38c4a70eb
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-07 12:05:08 +0530

    Time: 105 ms (43.15%), Space: 24 MB (38.78%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int mod=1e9+7;
-    vector<vector<int>> r={{1},{0,2},{0,1,3,4},{2,4},{0}};
-    
-    int solve(int n,int v,vector<vector<int>> &dp)
-    {
-        if(n==1)return 1;
-        if(dp[n][v]!=-1)return dp[n][v];
-        int ans=0;
-        for(int i:r[v])
-        {
-            ans=(ans+solve(n-1,i,dp))%mod;
-        }
-        return dp[n][v]=ans;
-    }
+    vector<vector<int>> r={{1,2,4},{0,2},{1,3},{2},{2,3}};
     int countVowelPermutation(int n) {
-        vector<vector<int>>dp(n+1,vector<int>(5,-1));
+        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        for(int z=0;z<5;z++)dp[1][z]=1;
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                for(int k:r[j])
+                {
+                    dp[i][j]=(dp[i][j]+dp[i-1][k])%mod;
+                }
+                // cout<<dp[i][j]<<" ";
+            }
+            // cout<<endl;
+        }
         int ans=0;
         for(int z=0;z<5;z++)
         {
-            ans=(ans+solve(n,z,dp))%mod;
+            ans=(ans+dp[n][z])%mod;
         }
         return ans;
     }
