4b554a739c8ce0c90bb0f55ab451b2e468fe42f4
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-08 17:39:13 +0530

    Time: 655 ms (7.98%), Space: 12.2 MB (59.15%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,54 +1,31 @@
 class Solution {
 public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp1(n,1);
+        vector<int> dp2(n,1);
     
-//     vector<int> longestsequence(vector<int>&nums)
-//     {
-//         vector<int>seq,ans;
-//         for(int i:nums)
-//         {
-//             auto it=lower_bound(seq.begin(),seq.end(),i);
-//             ans.push_back(it-seq.begin());
-//             if(it!=seq.end())*it=i;
-//             else seq.push_back(i);
-//         }
-//         return ans;
-//     }
-    
-//     int minimumMountainRemovals(vector<int>& nums) {
-        
-//         vector<int>is=longestsequence(nums);
-//         reverse(nums.begin(),nums.end());
-//         // for(int i:nums)cout<<i<<" ";
-//         vector<int>ds=longestsequence(nums);
-
-//         int len=INT_MAX;
-//         for(int z=1;z<nums.size();z++)
-//         {
-//             len=min(len,(int)nums.size()-(is[z]+ds[z]+1));
-//         }
-        
-//         return len;
-//     }
-        int minimumMountainRemovals(vector<int>& A) {
-        int N = A.size(), ans = N;
-        vector<int> a(N), b(N), v;
-        for (int i = 0 ; i < N; ++i) {
-            int x = A[i];
-            auto it = lower_bound(begin(v), end(v), x);
-            a[i] = it - begin(v);
-            if (it != end(v)) *it = x;
-            else v.push_back(x);
+        for(int z=0;z<n;z++)
+        {
+            for(int y=0;y<z;y++)
+            {
+                if(nums[z]>nums[y]&&dp1[z]<dp1[y]+1)
+                    dp1[z]=dp1[y]+1;
+            }
         }
-        v.clear();
-        for (int i = N - 1; i >= 0; --i) {
-            int x = A[i];
-            auto it = lower_bound(begin(v), end(v), x) ;
-            b[i] = it - begin(v); 
-            if (it != end(v)) *it = x;
-            else v.push_back(x);
+        for(int z=n-1;z>=0;z--)
+        {
+            for(int y=n-1;y>z;y--)
+            {
+                if(nums[z]>nums[y]&&dp2[z]<dp2[y]+1)
+                    dp2[z]=dp2[y]+1;
+            }
         }
-        for (int i = 1; i < N; ++i) {
-            if (a[i] && b[i]) ans = min(ans, N - (a[i] + b[i] + 1));
+        int ans=INT_MAX;
+        for(int z=1;z<n-1;z++)
+        {
+            if(dp1[z]!=1&&dp2[z]!=1)
+                ans=min(ans,n-(dp1[z]+dp2[z]-1));    
         }
         return ans;
     }
