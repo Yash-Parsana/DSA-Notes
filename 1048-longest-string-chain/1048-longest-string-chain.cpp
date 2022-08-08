2bb155cb383cae82da1f5efb98d5765840831df0
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-08 14:41:21 +0530

    Time: 264 ms (21.83%), Space: 27 MB (28.84%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    
     static bool cmp(string s1,string s2)
     {
         return s1.size()<s2.size();
+        
     }
-    
-    int longestStrChain(vector<string>& v) {
+    int longestStrChain(vector<string>& wd) {
+        map<string,int>dp;
         
-        unordered_map<string ,int> dp;
-        
-        sort(v.begin(),v.end(),cmp);
-        int ans=1;
-        for(int z=0;z<v.size();z++)
+        sort(wd.begin(),wd.end(),cmp);
+        int ans=0;
+        for(string s:wd)
         {
-            dp[v[z]]=1;
-            
-            for(int y=0;y<v[z].size();y++)
+            dp[s]=1;
+            for(int i=0;i<s.size();i++)
             {
-                string str=v[z].substr(0,y)+v[z].substr(y+1);
-                
-                if(dp.find(str)!=dp.end())
-                {
-                    dp[v[z]]=max(dp[v[z]],dp[str]+1);
-                }
+                string str=s.substr(0,i)+s.substr(i+1);
+                if(dp.find(str)!=dp.end()&&dp[s]<dp[str]+1)
+                    dp[s]=dp[str]+1;
             }
-            ans=max(ans,dp[v[z]]);
-            
+            ans=max(ans,dp[s]);
         }
         return ans;
-        
-        
-        
     }
 };
