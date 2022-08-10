ca2f36e61f4db45007941ab30ec6c27e0375b5c1
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-10 16:14:14 +0530

    Time: 14 ms (46.06%), Space: 8.5 MB (71.11%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -4,7 +4,7 @@ public:
         int n=v.size();
         vector<vector<int>>dp(n,vector<int>(n,0));
         
-        for(int i=n-1;i>=1;i--)
+        for(int i=n-1;i>0;i--)
         {
             for(int j=i+1;j<n;j++)
             {
