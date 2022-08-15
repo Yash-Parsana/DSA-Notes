9fef557e1dd1d9606e21a5f8c082a17b35a61477
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-08-15 14:58:31 +0530

    Time: 124 ms (33.26%), Space: 18.2 MB (39.60%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
- int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.empty()) {
-            return 0;
+    int maximalSquare(vector<vector<char>>& mtx) {
+        
+        int n=mtx.size(),m=mtx[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        int ans=0;
+        for(int z=0;z<n;z++)
+        {
+            dp[z][0]=mtx[z][0]-'0';
+            ans=max(ans,dp[z][0]);
         }
-        int m = matrix.size(), n = matrix[0].size(), sz = 0, pre;
-        vector<int> cur(n, 0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int temp = cur[j];
-                if (!i || !j || matrix[i][j] == '0') {
-                    cur[j] = matrix[i][j] - '0';
-                } else {
-                    cur[j] = min(pre, min(cur[j], cur[j - 1])) + 1;
+        for(int z=0;z<m;z++)
+        {
+            dp[0][z]=mtx[0][z]-'0';
+            ans=max(ans,dp[0][z]);
+        }
+        for(int z=1;z<n;z++)
+        {
+            for(int y=1;y<m;y++)
+            {
+                if(mtx[z][y]=='1')
+                {
+                    dp[z][y]=min({dp[z-1][y-1],dp[z][y-1],dp[z-1][y]})+1;
                 }
-                sz = max(cur[j], sz);
-                pre = temp;
+                ans=max(ans,dp[z][y]*dp[z][y]);
             }
         }
-        return sz * sz;
+        return ans;
+        
     }
 };
