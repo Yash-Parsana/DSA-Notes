bb296a941ada8baa926ed2f1f774583078131c52
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-03 15:17:47 +0530

    Time: 4 ms (58.32%), Space: 9.6 MB (43.49%) - LeetHub

--- 949-largest-time-for-given-digits/949-largest-time-for-given-digits.cpp
+++ 949-largest-time-for-given-digits/949-largest-time-for-given-digits.cpp
@@ -1,74 +1,88 @@
 class Solution {
 public:
     
-    void solve(int i,string temp,string &ans,vector<int> &v)
-    {
-        if(i==5)
-        {
-            if(ans=="")ans=temp;
-            else if(stoi(temp)>stoi(ans))
-                ans=temp;
-            return;
-        }
-        if(i==1)
-        {
-            for(char j='2';j>='0';j--)
-            {
-                if(v[j-'0'])
-                {
-                    v[j-'0']--;
-                    solve(i+1,temp+j,ans,v);
-                    v[j-'0']++;
-                }
-            }
-        }
-        else if(i==2)
-        {
-            char c='9';
-            if(temp=="2")
-            {
-                c='3';
-            }
-            for(c;c>='0';c--)
-            {
-                if(v[c-'0'])
-                {
-                    v[c-'0']--;
-                    solve(i+1,temp+c,ans,v);                    
-                    v[c-'0']++;
-                }
-            }
-        }
-        else
-        {
-            char c='9';
-            if(i==3)
-                c='5';
-            for(c;c>='0';c--)
-            {
-                if(v[c-'0'])
-                {
+//     void solve(int i,string temp,string &ans,vector<int> &v)
+//     {
+//         if(i==5)
+//         {
+//             if(ans=="")ans=temp;
+//             else if(stoi(temp)>stoi(ans))
+//                 ans=temp;
+//             return;
+//         }
+//         if(i==1)
+//         {
+//             for(char j='2';j>='0';j--)
+//             {
+//                 if(v[j-'0'])
+//                 {
+//                     v[j-'0']--;
+//                     solve(i+1,temp+j,ans,v);
+//                     v[j-'0']++;
+//                 }
+//             }
+//         }
+//         else if(i==2)
+//         {
+//             char c='9';
+//             if(temp=="2")
+//             {
+//                 c='3';
+//             }
+//             for(c;c>='0';c--)
+//             {
+//                 if(v[c-'0'])
+//                 {
+//                     v[c-'0']--;
+//                     solve(i+1,temp+c,ans,v);                    
+//                     v[c-'0']++;
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             char c='9';
+//             if(i==3)
+//                 c='5';
+//             for(c;c>='0';c--)
+//             {
+//                 if(v[c-'0'])
+//                 {
                     
-                        v[c-'0']--;
-                        solve(i+1,temp+c,ans,v);
-                        v[c-'0']++;
+//                         v[c-'0']--;
+//                         solve(i+1,temp+c,ans,v);
+//                         v[c-'0']++;
                     
-                }
-            }
-        }
+//                 }
+//             }
+//         }
         
-    }
+//     }
+    
+//     string largestTimeFromDigits(vector<int>& arr) {
+//         string ans="";
+//         vector<int>v(10,0);
+//         for(int i:arr)
+//             v[i]++;
+//         solve(1,"",ans,v);
+        
+//         if(ans.size()<4)return "";
+//         ans=ans.substr(0,2)+":"+ans.substr(2);
+        
+//         return ans;
+//     }
+    
     
     string largestTimeFromDigits(vector<int>& arr) {
-        string ans="";
-        vector<int>v(10,0);
-        for(int i:arr)
-            v[i]++;
-        solve(1,"",ans,v);
         
-        if(ans.size()<4)return "";
-        ans=ans.substr(0,2)+":"+ans.substr(2);
+        sort(arr.begin(),arr.end(),greater<int>());
         
-        return ans;
+        do{
+            if((arr[0]<2||arr[0]==2&&arr[1]<4)&&arr[2]<6)
+                return to_string(arr[0])+to_string(arr[1])+":"+to_string(arr[2])+to_string(arr[3]);
+        }
+        while(prev_permutation(arr.begin(),arr.end()));
+        
+        return "";
     }
 };
