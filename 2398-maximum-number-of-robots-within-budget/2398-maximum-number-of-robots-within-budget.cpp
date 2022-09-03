85daf96643cea7252f669297251c800b92170622
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-03 22:14:37 +0530

    Time: 443 ms (40.00%), Space: 109.3 MB (40.00%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,39 @@
+#define ll long long
+
+class Solution {
+public:
+    
+    
+    int maximumRobots(vector<int>& c, vector<int>& r, long long b) {
+        ll temp=b;
+        
+        int i=0,j=0,ans=0;
+        ll cost=0,sum=0;;
+        deque<int> dq;
+        while(j<c.size())
+        {
+            
+            if((dq.empty()&&c[j]+(j-i+1)*(sum+r[j])<=b)||!dq.empty()&&(max(c[dq.front()],c[j])+(j-i+1)*(sum+r[j]))<=b)
+            {
+                sum+=r[j];
+                while(!dq.empty()&&c[dq.back()]<c[j])dq.pop_back();
+
+                dq.push_back(j);
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            else if(i!=j)
+            {
+                if(!dq.empty()&&dq.front()==i)dq.pop_front();
+                sum-=r[i];
+                i++;           
+            }
+            else {
+                i++;j++;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
