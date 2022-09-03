cf55e3c5ae6a51c24ea1e60fddc7db1d48f327f1
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-03 22:59:55 +0530

    Time: 295 ms (100.00%), Space: 22 MB (100.00%) - LeetHub

---
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    void solve(vector<vector<int>>&v,vector<bool>vis,int i,int col,int &n,int &m,int &ans)
+    {
+        // if(i==m)return;
+        cout<<col<<" "<<i<<endl;
+        if(col==0||i==m)
+        {
+            int tempans=0;
+            
+            for(int j=0;j<n;j++)
+            {
+                bool flag=0;
+                for(int k=0;k<m;k++)
+                {
+                    if(v[j][k]&&!vis[k])
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+                if(!flag)
+                    tempans++;
+            }
+            ans=max(ans,tempans);
+            return ;
+        }
+        vis[i]=1;
+        solve(v,vis,i+1,col-1,n,m,ans);
+        vis[i]=0;
+        
+        solve(v,vis,i+1,col,n,m,ans);
+    }
+    
+    
+    int maximumRows(vector<vector<int>>& mat, int cols) {
+        
+        int n=mat.size(),m=mat[0].size();
+        int ans=-1;
+        
+        vector<bool> vis(m,0);
+        solve(mat,vis,0,cols,n,m,ans);
+        return ans;
+    }
+};
