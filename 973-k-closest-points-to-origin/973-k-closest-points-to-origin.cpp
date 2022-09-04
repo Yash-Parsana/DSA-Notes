fc81a1fbacd885b294b176112f7767a3f2884ece
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-04 17:43:50 +0530

    Time: 405 ms (52.02%), Space: 57.1 MB (89.67%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -6,7 +6,7 @@ public:
         return (pow(p1[0],2)+pow(p1[1],2))<(pow(p2[0],2)+pow(p2[1],2));
     }
     
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int &k) {
         
         sort(points.begin(),points.end(),cmp);
         
