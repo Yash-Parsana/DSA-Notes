ba4a36a2a53a288cb1d81ef6edf5f6e39ef3e927
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-08 17:58:51 +0530

    Time: 108 ms (18.27%), Space: 7.9 MB (48.95%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int balancedString(string s) {
+        
+        unordered_map<char,int>mp;
+        
+        int i=0,j=0,ans=INT_MAX,k=s.size()/4;
+        
+        for(char c:s)
+            mp[c]++;
+        
+        while(j<s.size())
+        {
+            
+            mp[s[j]]--;
+            while(i<s.size()&&mp['Q']<=k&&mp['W']<=k&&mp['E']<=k&&mp['R']<=k)
+            {
+                ans=min(ans,j-i+1);                
+                mp[s[i]]++;
+                i++;
+            }
+            j++;
+        }
+        if(ans<0)return 0;
+        return ans;
+    }
+};
