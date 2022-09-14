e6c4d607470152bc3af74d2e26e69a73f3cab7c0
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-14 09:53:57 +0530

    Time: 682 ms (37.56%), Space: 320.7 MB (19.48%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void solve(TreeNode*root,vector<int>v,int &ans)
+    {
+        if(!root->left&&!root->right)
+        {
+            v[root->val]++;
+            int odd=0;
+            for(int z=1;z<10;z++)
+            {
+                if(v[z]&1)odd++;
+            }
+            v[root->val]--;
+            if(odd<=1)
+            {
+                ans++;
+                // for(int i:v)
+                //     cout<<i<<" ";
+                // cout<<endl;
+            }
+            return;
+                
+        }
+        
+        v[root->val]++;
+        if(root->left)
+            solve(root->left,v,ans);
+        if(root->right)
+            solve(root->right,v,ans);
+        v[root->val]--;
+        
+    }
+    
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int>v(10,0);
+        int ans=0;
+        solve(root,v,ans);
+        return ans;
+    }
+};
