0bfec9d5de95d276ec80e9a2f427942936f5db59
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2022-09-16 09:31:41 +0530

    Time: 502 ms (80.91%), Space: 120 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     
-    int solve(vector<int>&multi,vector<int>&nums,int i,int j,int ii,vector<vector<int>>&dp)
+    int solve(vector<int>&multi,vector<int>&nums,int i,int ii,vector<vector<int>>&dp)
     {
         if(ii>=multi.size()||i>=multi.size())
             return 0;
         if(dp[ii][i]!=INT_MIN)return dp[ii][i];
         
-        return dp[ii][i]=max(solve(multi,nums,i+1,j,ii+1,dp)+multi[ii]*nums[i],solve(multi,nums,i,j-1,ii+1,dp)+multi[ii]*nums[j]);
+        int j=nums.size()-(ii-i)-1;
+        return dp[ii][i]=max(solve(multi,nums,i+1,ii+1,dp)+multi[ii]*nums[i],solve(multi,nums,i,ii+1,dp)+multi[ii]*nums[j]);
     }
     
     int maximumScore(vector<int>& nums, vector<int>& multi) {
         vector<vector<int>> dp(multi.size(),vector<int>(multi.size(),INT_MIN));
-        return solve(multi,nums,0,nums.size()-1,0,dp);
+        return solve(multi,nums,0,0,dp);
     }
 };
